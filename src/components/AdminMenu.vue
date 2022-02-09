2367e53ae969387775e8b174ac2599ce02f9a96e
wassimghommidh <wassghom8@gmail.com>
2022-02-09 14:48:30 +0100

    Menu food v1.09

--- src/components/AdminMenu.vue
+++ src/components/AdminMenu.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="menu">
     <h2>Add your Restaurant menu</h2>
-    <div  v-for="(input, index) in foodTypes"
-          :key="`foodInput-${index}`">
+    <div v-for="(input, index) in foodTypes" :key="`foodInput-${index}`">
       <label>Food name</label>
       <input
         type="text"
@@ -15,6 +14,7 @@
       <div class="col-4">
         <label>Price</label>
         <input
+          id="price"
           type="number"
           name="price"
           class="form-control"
@@ -23,8 +23,19 @@
         />
       </div>
     </div>
-    <button class="hello" @click="addField(input, foodTypes)">Add more</button>
-    <button class="hello" @click="removeField(input, foodTypes)">Add more</button>
+    <div style="text-align: center; margin-top: 30px">
+      <hr class="main-hr" />
+      <button
+        class="icon-btn add-btn"
+        id="hello"
+        @click="addField(input, foodTypes)"
+      >
+        <div class="btn-txt">Add</div>
+      </button>
+      <button class="icon-btn add-btn" @click="removeField(input, foodTypes)">
+        <div class="btn-txt">Remove</div>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -45,5 +56,101 @@ export default {
     },
   },
 };
-// app.addMore()
 </script>
+
+<style>
+.menu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#price {
+  margin-left: 45px;
+}
+
+.main-hr {
+  width: 30%;
+  border: none;
+  border-top: 1px solid #c3c3c3;
+}
+.icon-btn {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #cdcdcd;
+  background: white;
+  border-radius: 25px;
+  overflow: hidden;
+  position: relative;
+  transition: width 0.2s ease-in-out;
+}
+.add-btn:hover {
+  width: 120px;
+}
+.add-btn::before,
+.add-btn::after {
+  transition: width 0.2s ease-in-out, border-radius 0.2s ease-in-out;
+  content: "";
+  position: absolute;
+  height: 4px;
+  width: 10px;
+  top: calc(50% - 2px);
+  background: red;
+}
+.add-btn::after {
+  right: 14px;
+  overflow: hidden;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+.add-btn::before {
+  left: 14px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+.icon-btn:focus {
+  outline: none;
+}
+.btn-txt {
+  opacity: 0;
+  transition: opacity 0.2s;
+}
+.add-btn:hover::before,
+.add-btn:hover::after {
+  width: 4px;
+  border-radius: 2px;
+}
+.add-btn:hover .btn-txt {
+  opacity: 1;
+}
+.add-icon::after,
+.add-icon::before {
+  transition: all 0.2s ease-in-out;
+  content: "";
+  position: absolute;
+  height: 20px;
+  width: 2px;
+  top: calc(50% - 10px);
+  background: red;
+  overflow: hidden;
+}
+.add-icon::before {
+  left: 22px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+.add-icon::after {
+  right: 22px;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+.add-btn:hover .add-icon::before {
+  left: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+.add-btn:hover .add-icon::after {
+  right: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+</style>
